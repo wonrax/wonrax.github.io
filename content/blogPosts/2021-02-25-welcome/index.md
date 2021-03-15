924c001e04ff4a4b7bc590708abcdaf40887b571
wonrax <hahuylonghai2012@gmail.com>
2021-03-15 23:31:56 +0700

    added custom plugin for handling images

--- content/blogPosts/2021-02-25-welcome/index.md
+++ content/blogPosts/2021-02-25-welcome/index.md
@@ -4,7 +4,7 @@ date: 2021-02-25T23:18:00+07:00
 title: "Sed vehicula, arcu et faucibus volutpat, nulla risus porta velit?"
 excerpt: "Et id eum voluptas iste qui laudantium. Accusantium qui id voluptatem voluptas officia tempore ipsam deleniti rem. Est est occaecati voluptatem exercitationem ex possimus quo minima. Sit architecto est harum aut nemo perferendis reiciendis odio."
 --- 
-Morbi quis turpis eu mauris ultricies convallis vel venenatis risus. Maecenas posuere pretium ex quis ullamcorper. Ut ultricies ac ligula ac vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pharetra urna sapien, sit amet gravida ligula pharetra nec. Suspendisse sit amet aliquet felis.
+Morbi quis turpis eu mauris ultricies convallis vel venenatis risus. Maecenas posuere pretium ex quis ullamcorper. Ut ultricies ac ligula ac vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pharetra urna sapien, sit amet gravida ligula pharetra nec. Suspendisse sit emet aliquet felis.
 
 ![Figure 1: Phasellus semper, justo lobortis ullamcorper commodo, lacus diam hendrerit massa, eget laoreet enim ex vel metus](./windows.jpg)
 
--- plugins/gatsby-remark-unwrap-p-tags/index.js
+++ plugins/gatsby-remark-unwrap-p-tags/index.js
@@ -1,10 +1,28 @@
 const visit = require(`unist-util-visit`)
 const remove = require(`unist-util-remove`)
+const util = require("util")
+
+function toFigure(url, alt) {
+  return `<figure class="gatsby-resp-image-figure">
+    <span class="gatsby-resp-image-wrapper" style="position: relative; display: block; margin-left: auto; margin-right: auto;">
+      <a class="gatsby-resp-image-link" href="${url}" target="_blank" rel="noopener" style="display: block;">
+        <img class="gatsby-resp-image-image" alt="${alt}" src="${url}" style="max-width: 600px;">
+      </a>
+    </span>
+</figure>`
+}
 
 module.exports = ({ markdownAST }) => {
   visit(markdownAST, `paragraph`, (node, index, parent) => {
     const hasOnlyImagesNodes = node.children.every(child => {
-      return child.type === "html" && child.url && child.alt
+      if (child.type === "image") {
+        child["value"] = toFigure(child["url"], child["alt"])
+        child["type"] = "html"
+      }
+      return (
+        (child.type === "html" && child.url && child.alt) ||
+        child.type === "image"
+      )
     })
 
     if (!hasOnlyImagesNodes) {
@@ -12,9 +30,11 @@ module.exports = ({ markdownAST }) => {
     }
 
     remove(node, "text")
-    console.log(parent.children)
     parent.children.splice(index, 1, ...node.children)
-    console.log(parent.children)
+
+    console.log(
+      util.inspect(parent.children, false, null, true /* enable colors */)
+    )
 
     return index
   })
