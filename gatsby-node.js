61146fc2546c404b254a35613c7395e41b5846f7
wonrax <hahuylonghai2012@gmail.com>
2021-04-04 17:02:07 +0700

    fix react hot reload warning

--- gatsby-node.js
+++ gatsby-node.js
@@ -64,3 +64,13 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    }
+  }
+}
