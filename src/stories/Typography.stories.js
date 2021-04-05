20f73f24e3d7e90b444dd3811fc2b6344248a9d7
wonrax <hahuylonghai2012@gmail.com>
2021-04-05 22:07:11 +0700

    working storybook

---
+++ src/stories/Typography.stories.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Heading } from "../components"
+
+export default {
+  title: "Design system/Typography",
+}
+export const exampleStory = () => <Heading weight={3}>Hehe</Heading>
